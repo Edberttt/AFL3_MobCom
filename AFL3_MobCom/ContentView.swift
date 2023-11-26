0c7bec3b91c7125a15b2ad9e88038830e415a95a
Edbert <echandradinata@student.ciputra.ac.id>
2023-11-27 00:29:37 +0700

    Logic 90%

--- AFL3_MobCom/ContentView.swift
+++ AFL3_MobCom/ContentView.swift
@@ -7,12 +7,40 @@
 
 import SwiftUI
 
+struct Friend {
+    var name: String
+    var owe: String
+    var amount: String
+}
+
 struct ContentView:View {
     @State private var isAddViewPresented = false
-    
+    @State private var isFriendDetailPresented = false
     
     var body: some View {
         
+        let friends = [
+            Friend(name: "Friend 1", owe: "You Owe:", amount: "25000"),
+            Friend(name: "Friend 2", owe: "You Pay:", amount: "20000"),
+            Friend(name: "Friend 3", owe: "You Owe:", amount: "20000"),
+            Friend(name: "Friend 4", owe: "You Pay:", amount: "25000"),
+            Friend(name: "Friend 5", owe: "You Owe:", amount: "25000"),
+            Friend(name: "Friend 6", owe: "You Owe:", amount: "25000"),
+            Friend(name: "Friend 7", owe: "You Owe:", amount: "25000"),
+            Friend(name: "Friend 8", owe: "You Owe:", amount: "25000"),
+            Friend(name: "Friend 9", owe: "You Owe:", amount: "25000"),
+            Friend(name: "Friend 10", owe: "You Owe:", amount: "25000"),
+        ]
+        
+        let totalYouOwe = friends.reduce(0) { total, friend in
+            total + (friend.owe == "You Owe:" ? Int(friend.amount) ?? 0 : 0)
+        }
+
+        let totalFriendsOwe = friends.reduce(0) { total, friend in
+            total + (friend.owe == "You Pay:" ? Int(friend.amount) ?? 0 : 0)
+        }
+        
+        let totalFriendsYouOwe = friends.filter { $0.owe == "You Owe:" }.count
         
         ZStack() {
             Group {
@@ -20,15 +48,20 @@ struct ContentView:View {
                     .font(Font.custom("Open Sans", size: 32).weight(.semibold))
                     .foregroundColor(Color(red: 0.51, green: 0.39, blue: 0.85))
                     .offset(x: -86.50, y: -384)
+                    .padding(.top, 20)
                 ZStack() {
+                    
                     Text("Total Your Owe:")
                         .font(Font.custom("Open Sans", size: 15).weight(.semibold))
                         .foregroundColor(Color(red: 0.56, green: 0.54, blue: 0.54).opacity(0.90))
                         .offset(x: -59, y: -26.50)
-                    Text("IDR 75.000,00")
+                    
+                    Text("IDR \(totalYouOwe)")
                         .font(Font.custom("Open Sans", size: 36).weight(.semibold))
                         .foregroundColor(.black)
-                        .offset(x: 0, y: 12)
+                        .offset(x: -13, y: 12)
+
+                    
                 }
                 .offset(x: -37, y: -294.50)
                 ZStack() {
@@ -36,18 +69,21 @@ struct ContentView:View {
                         .font(Font.custom("Open Sans", size: 13).weight(.semibold))
                         .foregroundColor(Color(red: 0.56, green: 0.54, blue: 0.54).opacity(0.90))
                         .offset(x: 0, y: -16.50)
-                    Text("2")
+
+                    Text("\(totalFriendsYouOwe)")
                         .font(Font.custom("Open Sans", size: 20).weight(.semibold))
                         .foregroundColor(.black)
-                        .offset(x: -66, y: 12)
+                        .offset(x: 0, y: 12)
                 }
                 .offset(x: 100, y: -217.50)
                 ZStack() {
-                    Text("Total Your Friend’s Owe")
+                    
+                    Text("Total Your Friend’s Owe: ")
                         .font(Font.custom("Open Sans", size: 13).weight(.semibold))
                         .foregroundColor(Color(red: 0.56, green: 0.54, blue: 0.54).opacity(0.90))
                         .offset(x: 0, y: -16.50)
-                    Text("IDR 40.000,00")
+                    
+                    Text("IDR \(totalFriendsOwe)")
                         .font(Font.custom("Open Sans", size: 20).weight(.semibold))
                         .foregroundColor(.black)
                         .offset(x: -9.50, y: 12)
@@ -73,11 +109,6 @@ struct ContentView:View {
                         .cornerRadius(20)
                         .offset(x: 0, y: 0)
                     ZStack() {
-                        ZStack() {
-                            
-                        }
-                        .frame(width: 24, height: 24)
-                        .offset(x: -37.50, y: 0)
                         Text("Search")
                             .font(Font.custom("Open Sans", size: 16))
                             .foregroundColor(.white)
@@ -93,182 +124,95 @@ struct ContentView:View {
                             .padding(.leading, 15) // Increase leading padding
                     }
                     
+                    
+                    
                 }
                 .frame(width: 364, height: 38)
                 .offset(x: 0, y: -79)
-                ZStack() {
-                    Ellipse()
-                        .foregroundColor(.clear)
-                        .frame(width: 45, height: 45)
-                        .background(Color(red: 0.85, green: 0.85, blue: 0.85).opacity(0.90))
-                        .offset(x: -149, y: -6.50)
-                    Text("Friend 1")
-                        .font(Font.custom("Open Sans", size: 15).weight(.semibold))
-                        .foregroundColor(.white)
-                        .offset(x: -80, y: -7)
-                    Text("You Owe:       IDR 25.000,00")
-                        .font(Font.custom("PT Sans", size: 10))
-                        .foregroundColor(Color(red: 0.91, green: 0.14, blue: 0.14))
-                        .offset(x: 99, y: -8.50)
-                    Rectangle()
-                        .foregroundColor(.clear)
-                        .frame(width: 350, height: 0)
-                        .overlay(Rectangle()
-                            .stroke(.white, lineWidth: 0.50))
-                        .offset(x: -2.50, y: 29)
-                    ZStack() {
-                        
+                
+                
+                VStack{
+                    ScrollView{
+                        VStack {
+                            ForEach(friends, id: \.name) { friend in
+                                ZStack {
+                                    
+                                    Text(friend.name)
+                                        .font(Font.custom("Open Sans", size: 15).weight(.semibold))
+                                        .foregroundColor(.white)
+                                        .offset(x: -50, y: 0.50)
+                                    
+                                    VStack {
+                                        Text(friend.owe)
+                                            .font(Font.custom("PT Sans", size: 10))
+                                            .foregroundColor(friend.owe == "You Owe:" ? Color(red: 0.91, green: 0.14, blue: 0.14) : Color(red: 0.14, green: 0.70, blue: 0.09))
+                                        Text("IDR \(friend.amount)")
+                                            .font(Font.custom("PT Sans", size: 10))
+                                            .foregroundColor(friend.owe == "You Owe:" ? Color(red: 0.91, green: 0.14, blue: 0.14) : Color(red: 0.14, green: 0.70, blue: 0.09))
+                                    }
+                                    .offset(x: 80, y: 0.50)
+                                }
+                                .frame(width: 355, height: 58)
+        //                        .padding(.bottom)
+                            }
+                        }
                     }
-                    .frame(width: 24, height: 24)
-                    .offset(x: 165.50, y: -7)
+                    .frame(width:400, height: 580, alignment: .center)
                 }
-                .frame(width: 355, height: 58)
-                .offset(x: 0.50, y: 21)
-                ZStack() {
-                    Ellipse()
-                        .foregroundColor(.clear)
-                        .frame(width: 45, height: 45)
-                        .background(Color(red: 0.85, green: 0.85, blue: 0.85).opacity(0.90))
-                        .offset(x: -149, y: -6.50)
-                    Text("Friend 2")
-                        .font(Font.custom("Open Sans", size: 15).weight(.semibold))
-                        .foregroundColor(.white)
-                        .offset(x: -80, y: -7)
-                    Text("You Pay:       IDR 20.000,00")
-                        .font(Font.custom("PT Sans", size: 10))
-                        .foregroundColor(Color(red: 0.14, green: 0.70, blue: 0.09))
-                        .offset(x: 99, y: -8.50)
-                    Rectangle()
-                        .foregroundColor(.clear)
-                        .frame(width: 350, height: 0)
-                        .overlay(Rectangle()
-                            .stroke(.white, lineWidth: 0.50))
-                        .offset(x: -2.50, y: 29)
-                    ZStack() {
-                        
-                    }
-                    .frame(width: 24, height: 24)
-                    .offset(x: 165.50, y: -7)
-                }
-                .frame(width: 355, height: 58)
-                .offset(x: 0.50, y: 98)
-                ZStack() {
-                    Ellipse()
-                        .foregroundColor(.clear)
-                        .frame(width: 45, height: 45)
-                        .background(Color(red: 0.85, green: 0.85, blue: 0.85).opacity(0.90))
-                        .offset(x: -149, y: -6.50)
-                    Text("Friend 3")
-                        .font(Font.custom("Open Sans", size: 15).weight(.semibold))
-                        .foregroundColor(.white)
-                        .offset(x: -80, y: -7)
-                    Text("You Owe:       IDR 50.000,00")
-                        .font(Font.custom("PT Sans", size: 10))
-                        .foregroundColor(Color(red: 0.91, green: 0.14, blue: 0.14))
-                        .offset(x: 99, y: -8.50)
-                    Rectangle()
-                        .foregroundColor(.clear)
-                        .frame(width: 350, height: 0)
-                        .overlay(Rectangle()
-                            .stroke(.white, lineWidth: 0.50))
-                        .offset(x: -2.50, y: 29)
-                    ZStack() {
-                        
-                    }
-                    .frame(width: 24, height: 24)
-                    .offset(x: 165.50, y: -7)
-                }
-                .frame(width: 355, height: 58)
-                .offset(x: 0.50, y: 175)
+                .offset(x: 20, y: 250)
+                .frame(width:500, height: 580, alignment: .center)
+                
             }
-            Group {
-                ZStack() {
-                    Ellipse()
-                        .foregroundColor(.clear)
-                        .frame(width: 45, height: 45)
-                        .background(Color(red: 0.85, green: 0.85, blue: 0.85).opacity(0.90))
-                        .offset(x: -149, y: -6.50)
-                    Text("Friend 4")
-                        .font(Font.custom("Open Sans", size: 15).weight(.semibold))
-                        .foregroundColor(.white)
-                        .offset(x: -80, y: -7)
-                    Text("You Pay:       IDR 20.000,00")
-                        .font(Font.custom("PT Sans", size: 10))
-                        .foregroundColor(Color(red: 0.14, green: 0.70, blue: 0.09))
-                        .offset(x: 99, y: -8.50)
-                    Rectangle()
-                        .foregroundColor(.clear)
-                        .frame(width: 350, height: 0)
-                        .overlay(Rectangle()
-                            .stroke(.white, lineWidth: 0.50))
-                        .offset(x: -2.50, y: 29)
-                    ZStack() {
-                        
-                    }
-                    .frame(width: 24, height: 24)
-                    .offset(x: 165.50, y: -7)
-                }
-                .frame(width: 355, height: 58)
-                .offset(x: 0.50, y: 252)
-                Rectangle()
-                    .foregroundColor(.clear)
-                    .frame(width: 32.03, height: 31)
-                    .background(
-                        AsyncImage(url: URL(string: "https://via.placeholder.com/32x31"))
-                    )
-                    .offset(x: -148.98, y: 14.50)
-                Rectangle()
-                    .foregroundColor(.clear)
-                    .frame(width: 30, height: 31.20)
-                    .background(
-                        AsyncImage(url: URL(string: "https://via.placeholder.com/30x31"))
-                    )
-                    .offset(x: -149, y: 90.60)
-                Rectangle()
-                    .foregroundColor(.clear)
-                    .frame(width: 35, height: 35)
-                    .background(
-                        AsyncImage(url: URL(string: "https://via.placeholder.com/35x35"))
-                    )
-                    .offset(x: -150.50, y: 167.50)
-                Rectangle()
-                    .foregroundColor(.clear)
-                    .frame(width: 40, height: 60.74)
-                    .background(
-                        AsyncImage(url: URL(string: "https://via.placeholder.com/40x61"))
-                    )
-                    .offset(x: -148, y: 247.37)
-                
-                Text("+ Add New Friend")
-                    .font(Font.custom("Open Sans", size: 15).weight(.semibold))
-                    .foregroundColor(Color(red: 0.51, green: 0.39, blue: 0.85))
-                    .offset(x: 105, y: -127)
-                    .onTapGesture {
-                        isAddViewPresented = true
-                    }
-                    .sheet(isPresented: $isAddViewPresented) {
-                        AddView() // Ganti dengan nama Viewnya dsini
-                    }
-            }
-//            .navigationBarTitle("Friends")
-                
-                //                        NavigationLink(destination: AddView()) {
-                //                                    Text("+ Add New Friend")
-                //                                        .font(Font.custom("Open Sans", size: 15).weight(.semibold))
-                //                                        .foregroundColor(Color(red: 0.51, green: 0.39, blue: 0.85))
-                //                                        .offset(x: 105, y: -127)
-                //                                }
-                //
-                //                    }
-                
-                
             
+            Rectangle()
+                .foregroundColor(.clear)
+                .frame(width: 32.03, height: 31)
+                .background(
+                    AsyncImage(url: URL(string: "https://via.placeholder.com/32x31"))
+                )
+                .offset(x: -148.98, y: 14.50)
+            Rectangle()
+                .foregroundColor(.clear)
+                .frame(width: 30, height: 31.20)
+                .background(
+                    AsyncImage(url: URL(string: "https://via.placeholder.com/30x31"))
+                )
+                .offset(x: -149, y: 90.60)
+            Rectangle()
+                .foregroundColor(.clear)
+                .frame(width: 35, height: 35)
+                .background(
+                    AsyncImage(url: URL(string: "https://via.placeholder.com/35x35"))
+                )
+                .offset(x: -150.50, y: 167.50)
+            Rectangle()
+                .foregroundColor(.clear)
+                .frame(width: 40, height: 60.74)
+                .background(
+                    AsyncImage(url: URL(string: "https://via.placeholder.com/40x61"))
+                )
+                .offset(x: -148, y: 247.37)
+                    
+                    
+            Text("+ Add New Friend")
+                .font(Font.custom("Open Sans", size: 15).weight(.semibold))
+                .foregroundColor(Color(red: 0.51, green: 0.39, blue: 0.85))
+                .offset(x: 105, y: -127)
+                .onTapGesture {
+                    isAddViewPresented = true
+                }
+                .sheet(isPresented: $isAddViewPresented) {
+                    friendDetails() // Ganti dengan nama Viewnya dsini
+                }
+                
+                
+            }
+            .frame(width: 430, height: 932)
+            .background(.white)
+            .padding(.top, 100)
         }
-        .frame(width: 430, height: 932)
-        .background(.white)
-    }
+        
 }
-
 
 
 #Preview {
